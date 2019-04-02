ca8bcd0e0456789097759749913b09c1975b559f
Hélio Augusto Sabóia Moura <helioasmoura@gmail.com>
2019-04-02 08:50:39 -0300

    Melhorada a escrita do código e passou a usar FreeSpec nos testes.

--- src/main/scala/br/com/hmsoftware/brainfuck/BrainFuck.scala
+++ src/main/scala/br/com/hmsoftware/brainfuck/BrainFuck.scala
@@ -4,143 +4,125 @@ package br.com.hmsoftware.brainfuck
   *
   * Comandos da linguagem BrainFuck:
   *
-  *   ,  Le um byte e o coloca na posiçao corrente da memoria.
-  *   .  Imprime o byte da posiçao corrente da memoria.
-  *   >  Avança uma posiçao na memoria.
-  *   <  Recua uma posiçao na memoria.
-  *   +  Incrementa a posiçao corrente da memoria de 1.
-  *   -  Decrementa a posiçao corrente da memoria de 1.
-  *   [  Se a posiçao corrente da memoria for diferente de zero vai para o proximo comando
-  *      senao vai para o comando seguinte ao ']' casado com este '['.
-  *   ]  Se a posiçao corrente da memoria for igual a zero vai para o proximo comando
-  *      senao vai para o comando seguinte ao '[' casado com este ']'.
+  *   ,  Lê um byte e o coloca na posição corrente da memória.
+  *   .  Imprime o byte da posição corrente da memória.
+  *   >  Avança uma posição na memória.
+  *   <  Recua uma posição na memória.
+  *   +  Incrementa a posição corrente da memória de 1.
+  *   -  Decrementa a posição corrente da memória de 1.
+  *   [  Se a posição corrente da memória for diferente de zero vai para o próximo comando
+  *      senão vai para o comando seguinte ao ']' casado com este '['.
+  *   ]  Se a posição corrente da memória for igual a zero vai para o próximo comando
+  *      senão vai para o comando seguinte ao '[' casado com este ']'.
   *
   *   Ignora qualquer outro caractere, inclusive quebra de linhas, permitindo a inclusao de comentarios a vontade.
   *
-  * Helio Moura - helioasmoura@gmail.com
-  */
+  * Helio Moura - helioasmoura@gmail.com */
 class BrainFuck {
 
-  /** Funçao para ler um byte da entrada. */
-  private var ent: Option[() => Byte] = None
+  /** Função para ler um byte da entrada. */
+  private var ent: Option[ () => Byte ] = None
 
-  /** Funçao para escrever um byte na saida. */
-  private var sai: Option[Byte => Unit] = None
+  /** Função para escrever um byte na saída. */
+  private var sai: Option[ Byte => Unit ] = None
 
-  /** Funçao para imprimir um texto de depuracao. */
-  private var dep: Option[String => Unit] = None
+  /** Função para imprimir um texto de depuração. */
+  private var dep: Option[ String => Unit ] = None
 
-  /** Memoria (cresce conforme seu uso). */
-  private val mem = collection.mutable.ArrayBuffer[Byte]()
+  /** memória (cresce conforme seu uso). */
+  private val mem = collection.mutable.ArrayBuffer[ Byte ]()
 
-  /** Posicao corrente da memoria. */
+  /** Posicao corrente da memória. */
   private var kmem = 0
 
-  /** Codigo executavel compilado. */
-  private var exe: Array[Char] = Array()
+  /** Código executável compilado. */
+  private var exe = Array[ Char ]()
 
-  /** Posicao corrente do codigo executavel. */
+  /** Posicao corrente do código executável. */
   private var kexe = 0
 
-  /** Pilha de laços ativos. */
-  private val pLacos = collection.mutable.Stack[Int]()
+  /** Pilha de laços ativos.
+    * Seu topo contém a posição inicial do laço corrente. */
+  private val pLacos = collection.mutable.Stack[ Int ]()
 
   /** Contador de passos do processador. */
   private var passo = 0
 
-  /** Definiçao da funçao de entrada.
-    * @param f Funçao de entrada a ser utilizada.
-    * @return Este proprio objeto, permitindo chamada encadeada.
-    */
-  def entrada(f: Option[() => Byte]) = { ent = f; this }
+  /** Definição da função de entrada.
+    * @param f Função de entrada a ser utilizada.
+    * @return Este próprio objeto, permitindo chamada encadeada. */
+  def entrada( f: Option[ () => Byte ] ) = { ent = f; this }
 
-  /** Definiçao da funçao de saida.
-    * @param f Funçao de saida a ser utilizada.
-    * @return Este proprio objeto, permitindo chamada encadeada.
-    */
-  def saida(f: Option[Byte => Unit]) = { sai = f; this }
+  /** Definição da função de saída.
+    * @param f Função de saída a ser utilizada.
+    * @return Este próprio objeto, permitindo chamada encadeada. */
+  def saida( f: Option[ Byte => Unit ] ) = { sai = f; this }
 
-  /** Definiçao da funçao de depuraçao.
-    * @param f Funçao de depuraçao a ser utilizada.
-    * @return Este proprio objeto, permitindo chamada encadeada.
-    */
-  def depurar(f: Option[String => Unit]) = { dep = f; this }
+  /** Definição da função de depuração.
+    * @param f Função de depuração a ser utilizada.
+    * @return Este próprio objeto, permitindo chamada encadeada. */
+  def depurar( f: Option[ String => Unit ] ) = { dep = f; this }
 
-  /** Comanda a execuçao de um programa BrainFuck.
+  /** Comanda a execução de um programa BrainFuck.
     * @param programa Programa BrainFuck a ser executado.
-    * @return Este proprio objeto, permitindo chamada encadeada.
-    */
-  def executar(programa: String) = {
+    * @return Este proprio objeto, permitindo chamada encadeada. */
+  def executar( programa: String ) = {
     ent match {
-      case Some(e) =>
+      case Some( e ) =>
         sai match {
-          case Some(s) => compilar(programa); zerar; rodar(e, s)
-          case None => throw new Exception("Saida nao foi definida.")
-        }
-      case None => throw new Exception("Entrada nao foi definida.")
-    }
-    this
-  }
+          case Some( s ) => compilar( programa ); zerar; rodar( e, s )
+          case None => throw new Exception( "Saida nao foi definida." ) }
+      case None => throw new Exception("Entrada nao foi definida.") }
+    this }
 
-  /** Compila um programa BrainFuck retirando comentarios e movendo o codigo para um vetor.
-    * @param prg Programa BrainFuck a ser executado.
-    */
-  private def compilar(prg: String) = exe = (prg filter ("[]+-<>,." contains _)).toArray
+  /** Compila um programa BrainFuck retirando comentários e movendo o código para um vetor.
+    * @param prg Programa BrainFuck a ser executado. */
+  private def compilar( prg: String ) = exe = ( prg filter ( "[]+-<>,." contains _ ) ).toArray
 
   /** Reinicia o processador de BrainFuck. */
   private def zerar = { mem.clear; mem += 0; kmem = 0; pLacos.clear; kexe = 0; passo = 0 }
 
-  /** Busca a saida do laço corrente. */
-  private def saiLaco(n: Int = 0): Unit =
-    if (exe(kexe) != ']')
-      if (exe(kexe) == '[') { kexe += 1; saiLaco(n + 1) }
-      else { kexe += 1; saiLaco(n) }
+  /** Busca a saída do laço corrente. */
+  private def saiLaco( n: Int = 0 ): Unit =
+    if ( exe( kexe ) != ']' )
+      if ( exe( kexe ) == '[' ) { kexe += 1; saiLaco( n + 1 ) }
+      else { kexe += 1; saiLaco( n ) }
     else
-      if (n > 0) { kexe += 1; saiLaco(n - 1) }
+      if ( n > 0 ) { kexe += 1; saiLaco( n - 1 ) }
       else { pLacos.pop; kexe += 1 }
 
   /** Interpretador de BrainFuck.
-    * @param e Funçao de Entrada.
-    * @param s Funçao de Saida.
-    */
+    * @param e Função de Entrada.
+    * @param s Função de Saida. */
   private def rodar(e: () => Byte, s: Byte => Unit): Unit = {
     passo += 1
-    if (passo < 100 && kexe < exe.length) {
+    if ( passo < 100 && kexe < exe.length ) {
       dep match {
-        case Some(d) => d(s"$passo# p($kexe):${exe(kexe)} m($kmem):${mem(kmem)}")
-        case None =>
-      }
+        case Some(d) => d( s"$passo# p($kexe):${ exe( kexe ) } m($kmem):${ mem( kmem ) }" )
+        case None => }
       exe(kexe) match {
         case '[' =>
-          if (mem(kmem) != 0) {
-            kexe += 1; pLacos push kexe
-          }
+          if ( mem( kmem ) != 0 ) { kexe += 1; pLacos push kexe }
           else saiLaco()
         case ']' =>
-          if (mem(kmem) != 0) kexe = pLacos.top
-          else {
-            kexe += 1; pLacos.pop
-          }
+          if ( mem( kmem ) != 0 ) kexe = pLacos.top
+          else { kexe += 1; pLacos.pop }
         case '+' =>
-          mem(kmem) = (mem(kmem) + 1).toByte
+          mem( kmem ) = ( mem( kmem ) + 1 ).toByte
           kexe += 1
         case '-' =>
-          mem(kmem) = (mem(kmem) - 1).toByte
+          mem( kmem ) = ( mem( kmem ) - 1 ).toByte
           kexe += 1
         case '<' =>
-          if (kmem > 0) kmem -= 1
+          if ( kmem > 0 ) kmem -= 1
           kexe += 1
         case '>' =>
           kmem += 1
-          if (kmem == mem.length) mem += 0
+          if ( kmem == mem.length ) mem += 0
           kexe += 1
         case '.' =>
-          s(mem(kmem)); kexe += 1
+          s( mem( kmem ) ); kexe += 1
         case ',' =>
-          mem(kmem) = e(); kexe += 1
-      }
-      rodar(e, s)
-    }
-  }
-}
+          mem( kmem ) = e(); kexe += 1 }
+      rodar( e, s ) } } }
 
--- src/test/scala/br/com/hmsoftware/brainfuck/BrainFuckSpec.scala
+++ src/test/scala/br/com/hmsoftware/brainfuck/BrainFuckSpec.scala
@@ -4,36 +4,45 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest._
 import collection.mutable.ListBuffer
 
-class BrainFuckSpec extends AsyncWordSpec with Matchers with ScalaFutures {
+class BrainFuckSpec extends FreeSpec with Matchers with ScalaFutures {
 
-  "BrainSpec" when {
-    "Executa simplesmente" in {
+  "BrainSpec" - {
+
+    "Executa o incremento de um valor informado na entrada" in {
+
       val w = new BrainFuck
-      w entrada Some(() => 13.toByte)
-      var ss = ListBuffer[Byte]()
-      w saida Some(s => { ss += s; println(s"###=====>>> $s") })
-      w depurar Some(s => println(s))
-      w executar (",+.")
-      ss shouldEqual List(14)
-    }
+      w entrada Some( () => 13.toByte )
+      var ss = ListBuffer[ Byte ]()
+      w saida Some( s => { ss += s; println( s"###=====>>> $s" ) } )
+      w depurar Some( s => println( s ) )
+      w executar ",+."
+      ss shouldEqual List( 14 ) }
 
-    "Executa simplesmente 2" in {
-      var ss = ListBuffer[Byte]()
-      val w = new BrainFuck entrada Some(() => 13.toByte) saida Some(s => { ss += s; println(s"###=====>>> $s") }) depurar Some(s => println(s))
-      w executar (",[->+<].>.")
-      ss shouldEqual List(0, 13)
-    }
+    "Executa o somatório de zero a um valor infromado na entrada." in {
+
+      var ss = ListBuffer[ Byte ]()
+      val w = new BrainFuck entrada Some( 
+        () => 13.toByte 
+      ) saida Some( 
+        s => { ss += s; println( s"###=====>>> $s" ) } 
+      ) depurar Some( 
+        s => println(s) 
+      )
+      w executar ",[->+<].>."
+      ss shouldEqual List( 0, 13 ) }
 
     "Executa simplesmente 3" in {
-      var ss = ListBuffer[Byte]()
-      var d = List(1, 2, 3, 4, 5)
-      val w = new BrainFuck
-      w entrada Some(() => if (d.length > 0) { val x = d.head.toByte; d = d.tail; x } else 0.toByte)
-      w saida Some(s => { ss += s; println(s"###=====>>> $s") })
-      w depurar Some(s => println(s))
-      w executar (",[[->+<]>.<,]>.")
-      ss shouldEqual List(1, 3, 6, 10)
-    }
-  }
 
-}
+      var ss = ListBuffer[ Byte ]()
+      var d = List( 1, 2, 3, 4, 5 )
+      val w = new BrainFuck
+      w entrada Some( () => 
+        if ( d.length > 0 ) { 
+          val x = d.head.toByte; d = d.tail; x } 
+        else 
+          0.toByte )
+      w saida Some( s => { ss += s; println( s"###=====>>> $s" ) } )
+      w depurar Some( s => println( s ) )
+      w executar ",[[->+<]>.<,]>."
+      ss shouldEqual List( 1, 3, 6, 10 ) } } }
+
